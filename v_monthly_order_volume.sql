846355915e7fe2ed92dc89f59be26217c9a2af24
Grzegorz Jewusiak <grzegorz@jewusiak.pl>
2025-11-05 13:51:03 +0100

    Modify view to include currency_id in monthly order volume

--- v_monthly_order_volume.sql
+++ v_monthly_order_volume.sql
@@ -2,12 +2,12 @@
 create or replace view cfr.v_monthly_order_volume as
 select
     to_char(o.purchase_date, 'YYYY-MM') as order_month,
-    o.currency,
+    o.currency_id,
     sum(o.total) as monthly_sales_volume,
     count(o.id) as monthly_order_count
 from
     cfr.orders o
 group by
-    order_month, o.currency
+    order_month, o.currency_id
 order by
-    order_month, o.currency;
+    order_month, o.currency_id;
